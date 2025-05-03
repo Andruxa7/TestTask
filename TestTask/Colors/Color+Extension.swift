072c4274bd22ff15ea790cbf68a6d8cbf921cc44
Andrii Stetsenko <a.stecenko7@gmail.com>
2025-05-03 16:56:39 +0300

    Added colors in Assets

---
+++ TestTask/Colors/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extension.swift
+//  TestTask
+//
+//  Created by Andrii Stetsenko on 03.05.2025.
+//
+
+import Foundation
+import SwiftUI
+
+public extension Color {
+    static let primary = Color("Primary", bundle: .main)
+    static let secondary = Color("Secondary", bundle: .main)
+    static let background1 = Color("Background", bundle: .main)
+    static let buttonDisabled1 = Color("ButtonDisabled", bundle: .main)
+}
