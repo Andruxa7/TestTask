a327e0ceb0f4c05c027f8e970e39bdacd0003bdf
Andrii Stetsenko <a.stecenko7@gmail.com>
2025-05-03 14:58:34 +0300

    Initial commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Andrii Stetsenko on 03.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
