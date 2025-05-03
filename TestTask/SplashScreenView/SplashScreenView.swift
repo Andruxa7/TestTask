1ad3b2ac1be7cd8c6f26895bf4afacfacafc0080
Andrii Stetsenko <a.stecenko7@gmail.com>
2025-05-03 17:10:25 +0300

    Added a SplashScreenView

---
+++ TestTask/SplashScreenView/SplashScreenView.swift
@@ -0,0 +1,26 @@
+//
+//  SplashScreenView.swift
+//  TestTask
+//
+//  Created by Andrii Stetsenko on 03.05.2025.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        ZStack {
+            Color.primary.ignoresSafeArea()
+            VStack {
+                Image("logoTestTask")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 160, height: 106.46)
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
--- TestTask/TestTaskApp.swift
+++ TestTask/TestTaskApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct TestTaskApp: App {
+    @State private var showSplash = true
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if showSplash {
+                SplashScreenView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation {
+                                showSplash = false
+                            }
+                        }
+                    }
+            } else {
+                ContentView()
+            }
         }
     }
 }
