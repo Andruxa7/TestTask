42e3c7ea3b74e6009c65385d69749664ce7f8419
Andrii Stetsenko <a.stecenko7@gmail.com>
2025-05-03 22:58:42 +0300

    Added API
    - created Environment
    - created Users

---
+++ TestTask/API/Environment.swift
@@ -0,0 +1,17 @@
+//
+//  Environment.swift
+//  TestTask
+//
+//  Created by Andrii Stetsenko on 03.05.2025.
+//
+
+import Foundation
+
+struct Environment {
+    static let baseURL = "https://frontend-test-assignment-api.abz.agency"
+    static let usersPath = "/api/v1/users"
+    static let pageParam = "page"
+    static let countParam = "count"
+    
+    static let countItems: Int = 6
+}
---
+++ TestTask/API/Users.swift
@@ -0,0 +1,68 @@
+//
+//  Users.swift
+//  TestTask
+//
+//  Created by Andrii Stetsenko on 03.05.2025.
+//
+
+import Foundation
+
+// MARK: - Users
+struct Users: Codable {
+    let success: Bool
+    let totalPages, totalUsers, count, page: Int
+    let links: Links
+    let users: [User]
+
+    enum CodingKeys: String, CodingKey {
+        case success
+        case totalPages = "total_pages"
+        case totalUsers = "total_users"
+        case count, page, links, users
+    }
+}
+
+// MARK: - Links
+struct Links: Codable {
+    let nextURL: String?
+    let prevURL: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case nextURL = "next_url"
+        case prevURL = "prev_url"
+    }
+}
+
+// MARK: - User
+struct User: Codable, Identifiable {
+    let id: Int
+    let name, email, phone: String
+    let position: String
+    let positionID, registrationTimestamp: Int
+    let photo: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, email, phone, position
+        case positionID = "position_id"
+        case registrationTimestamp = "registration_timestamp"
+        case photo
+    }
+}
+
+// MARK: - Positions
+struct Positions: Codable {
+    let success: Bool
+    let positions: [Position]
+}
+
+// MARK: - Position
+struct Position: Codable {
+    let id: Int
+    let name: String
+}
+
+// MARK: - Token
+struct Token: Codable {
+    let success: Bool
+    let token: String
+}
