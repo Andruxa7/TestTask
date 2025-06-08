73abb9b0ef992e40c828fde47374a3bc3b98c3f1
Andrii Stetsenko <a.stecenko7@gmail.com>
2025-06-09 00:36:07 +0300

    Added more images in Assets

--- TestTask/TestTaskApp.swift
+++ TestTask/TestTaskApp.swift
@@ -17,7 +17,7 @@ struct TestTaskApp: App {
             if showSplash {
                 SplashScreenView()
                     .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                             withAnimation {
                                 showSplash = false
                             }
